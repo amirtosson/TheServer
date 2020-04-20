c2202d9a81a1de9e2f1f3b20860b9a7fb0730b98
Amir <at@conze.com>
2020-04-20 09:09:48 +0200

    Server name

--- bbws/main.cpp
+++ bbws/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void main()
 {
 	WebServer webServer("127.0.0.1", 54000);
-	cerr << "Welcome to SoFa sever \nThe server is running on port # " << 54000 << endl;
+	cerr << "Welcome to the sever \nThe server is running on port # " << 54000 << endl;
 	while (true)
 	{
 		if (webServer.init() != 0)
